fe76869fe0ca1a86d32543187cefe4a658a40744
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2020-01-15 13:58:31 -0500

    Added code to download pre-trained models

---
+++ src/python/codequestion/download.py
@@ -0,0 +1,72 @@
+"""
+Download module
+"""
+
+import os.path
+import zipfile
+
+from urllib.request import urlopen
+
+from tqdm import tqdm
+
+# pylint: disable = E0401
+from .models import Models
+
+class Download(object):
+    """
+    Downloads a pre-trained model.
+    """
+
+    @staticmethod
+    def download(url, dest):
+        """
+        Downloads a remote file from url and stores at dest.
+
+        Args:
+            url: remote url
+            dest: destination file path
+        """
+
+        with urlopen(url) as response:
+            buffer = 16 * 1024
+            size = int(response.info()["Content-Length"])
+
+            with tqdm(total=size, unit="B", unit_scale=True, unit_divisor=1024) as pbar:
+                with open(dest, "wb") as f:
+                    while True:
+                        chunk = response.read(buffer)
+                        if not chunk:
+                            break
+
+                        f.write(chunk)
+                        pbar.update(len(chunk))
+
+    @staticmethod
+    def run(url):
+        """
+        Downloads a pre-trained model from url into the local model cache directory.
+
+        Args:
+            url: url model path
+        """
+
+        # Get base models path
+        path = Models.basePath(True)
+        dest = os.path.join(path, os.path.basename(url))
+
+        print("Downloading model from %s to %s" % (url, dest))
+
+        # Download file
+        Download.download(url, dest)
+
+        print("\nDecompressing model")
+
+        # Ensure file was downloaded successfully
+        if os.path.exists(dest):
+            with zipfile.ZipFile(dest, "r") as z:
+                z.extractall(path)
+
+        print("Download complete")
+
+if __name__ == "__main__":
+    Download.run("file:///tmp/cqmodel.zip")
