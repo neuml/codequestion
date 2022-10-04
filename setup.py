878b82b4927880fb2ab1e5d18c7131e5d9fa3d7d
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2022-10-04 10:16:19 -0400

    Bump version, upgrade libraries, closes #21

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 # pylint: disable = C0111
 from setuptools import setup
 
-with open("README.md", "r") as f:
+with open("README.md", "r", encoding="utf-8") as f:
     DESCRIPTION = f.read()
 
 setup(name="codequestion",
-      version="1.4.0",
+      version="2.0.0",
       author="NeuML",
       description="Ask coding questions directly from the terminal",
       long_description=DESCRIPTION,
@@ -21,7 +21,7 @@ setup(name="codequestion",
       packages=["codequestion"],
       package_dir={"": "src/python/"},
       keywords="python search embedding machine-learning",
-      python_requires=">=3.6",
+      python_requires=">=3.7",
       entry_points={
           "console_scripts": [
               "codequestion = codequestion.shell:main",
@@ -29,9 +29,10 @@ setup(name="codequestion",
       },
       install_requires=[
           "html2markdown>=0.1.7",
-          "mdv>=1.7.4",
-          "tqdm==4.48.0",
-          "txtai[similarity]>=3.2.0"
+          "rich>=12.0.1",
+          "scipy>=1.4.1",
+          "tqdm>=4.48.0",
+          "txtai[similarity]>=5.0.0"
       ],
       classifiers=[
           "License :: OSI Approved :: Apache Software License",
