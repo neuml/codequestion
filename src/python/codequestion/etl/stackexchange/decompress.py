306addf9aab1e5f3c1432af81d57175caaef8236
davidmezzetti <561939+davidmezzetti@users.noreply.github.com>
2022-10-05 05:35:57 -0400

    Update decompress step

--- src/python/codequestion/etl/stackexchange/decompress.py
+++ src/python/codequestion/etl/stackexchange/decompress.py
@@ -20,9 +20,9 @@ class Decompress:
             path: input directory path with 7z files
         """
 
-        # Check for either 7za or 7z executable
+        # Resolve full path to 7za or 7z
         binary = shutil.which("7za")
-        binary = binary if binary else "7z"
+        binary = binary if binary else shutil.which("7z")
 
         # Build command
         command = f"{binary} e {path}/*.7z Posts.xml -y -o{path}"
